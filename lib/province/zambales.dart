02d24e33840c048c010fc47a077f106d2733239e
Rhea Tamera <rhea.tamerarj@gmail.com>
2021-05-09 02:29:36 +0400

    edited zambales.dart

--- lib/province/zambales.dart
+++ lib/province/zambales.dart
@@ -32,47 +32,50 @@ class _ShowZambalesDataState extends State<ShowZambalesData> {
   }
 
   Widget build(BuildContext context) {
-    _loadCSV();
-    return Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          title: Text("Cities/Municipalities"),
-          backgroundColor: Colors.blue.shade900,
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: Column(
+  return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text("Cities/Municipalities"),
+        backgroundColor: Colors.blue.shade900,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SafeArea(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               Expanded(
-                  child: ListView.builder(
-                      itemCount: _data == null ? 0 : _data.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return new InkWell(
-                            onTap: () {
-                              Navigator.push(context,
-                                  new MaterialPageRoute(builder: (context) {
-                                return new SingleChildScrollView(
-                                    child: new Card(
-                                  margin: EdgeInsets.symmetric(horizontal: 20),
-                                  elevation: 8,
+                child: ListView.builder(
+                    itemCount: _data == null ? 0 : _data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return new InkWell(
+                        onTap: () {
+                          Navigator.push(context,
+                              new MaterialPageRoute(builder: (context) {
+                            return new SingleChildScrollView(
+                                child: new Card(
+                              //margin: EdgeInsets.symmetric(horizontal: 10), squishes it :/
+                              elevation: 8,
+                              child: SafeArea(
                                   child: Column(
-                                    children: <Widget>[
-                                      Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(15.0),
+                                      child: Row(
                                         children: <Widget>[
                                           RichText(
                                             text: TextSpan(
                                               children: [
                                                 TextSpan(
                                                   text: "Case Update\n",
-                                                  style: kTitleTextstyle,
+                                                  style: kTitleTextstyle4,
                                                 ),
                                                 TextSpan(
-                                                  text:
-                                                      "Newest update May 8, 2021",
+                                                  text: "Newest update May 8, 2021",
                                                   style: TextStyle(
                                                     color: kTextLightColor,
+                                                    fontSize: 15.0,
                                                   ),
                                                 ),
                                               ],
@@ -88,141 +91,133 @@ class _ShowZambalesDataState extends State<ShowZambalesData> {
                                           ),
                                         ],
                                       ),
-                                      SizedBox(height: 20),
-                                      Container(
-                                        padding: EdgeInsets.all(20),
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                          color: Colors.white,
-                                          boxShadow: [
-                                            BoxShadow(
-                                              offset: Offset(0, 4),
-                                              blurRadius: 30,
-                                              color: kShadowColor,
-                                            ),
-                                          ],
-                                        ),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            //Case updates card
-                                            Counter(
-                                              color: kInfectedColor,
-                                              number: _data[index][9],
-                                              title: "Active",
-                                            ),
-                                            Counter(
-                                              color: kDeathColor,
-                                              number: _data[index][10],
-                                              title: "Deaths",
-                                            ),
-                                            Counter(
-                                              color: kRecovercolor,
-                                              number: _data[index][11],
-                                              title: "Recovered",
-                                            ),
-                                            Counter(
-                                              color: kPrimaryColor,
-                                              number: _data[index][12],
-                                              title: "Total Cases",
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(height: 20),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: <Widget>[
-                                          Text(
-                                            _data[index][2]
-                                                .toString()
-                                                .toUpperCase(),
-                                            style: kTitleTextstyle,
+                                    ),
+                                    SizedBox(height: 20),
+                                    Container(
+                                      padding: EdgeInsets.all(20),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.white,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            offset: Offset(0, 4),
+                                            blurRadius: 30,
+                                            color: kShadowColor,
                                           ),
                                         ],
                                       ),
-                                      SizedBox(height: 20),
-                                      Row(
+                                      child: Row(
                                         mainAxisAlignment:
                                             MainAxisAlignment.spaceBetween,
                                         children: <Widget>[
-                                          Text(
-                                            "TRAVEL REQUIREMENTS",
-                                            style: kTitleTextstyle,
+                                          //Case updates card
+                                          Counter(
+                                            color: kInfectedColor,
+                                            number: _data[index][9],
+                                            title: "Active",
+                                          ),
+                                          Counter(
+                                            color: kDeathColor,
+                                            number: _data[index][10],
+                                            title: "Deaths",
+                                          ),
+                                          Counter(
+                                            color: kRecovercolor,
+                                            number: _data[index][11],
+                                            title: "Recovered",
+                                          ),
+                                          Counter(
+                                            color: kPrimaryColor,
+                                            number: _data[index][12],
+                                            title: "Total Cases",
                                           ),
                                         ],
                                       ),
-                                      SizedBox(height: 20),
-                                      Text(_data[index][3],
-                                          style: kTitleTextstyle2),
-                                      SizedBox(height: 10),
-                                      //Travel Requirements card
-                                      TravelCard(
-                                        text: _data[index][4].toString(),
-                                        image: "images/2.png",
-                                        title: "Requirement # 1",
-                                      ),
-                                      TravelCard(
-                                        text: _data[index][5].toString(),
-                                        image: "/images/1.png",
-                                        title: "Requirement # 2",
-                                      ),
-                                      TravelCard(
-                                        text: _data[index][6].toString(),
-                                        image: "/images/5.png",
-                                        title: "Requirement # 3",
-                                      ),
-                                      TravelCard(
-                                        text: _data[index][7].toString(),
-                                        image: "/images/3.png",
-                                        title: "Requirement # 4",
-                                      ),
-                                      TravelCard(
-                                        text: _data[index][8].toString(),
-                                        image: "/images/4.png",
-                                        title: "Requirement # 5",
-                                      ),
-                                      SizedBox(height: 30),
-                                    ],
-                                  ),
-                                ),
-                                );
-                              },
-                              ),
-                              );
-                            },
-                            //create cards for the cities
-                            child: new Card(
-                              elevation: 20,
-                              color: Colors.teal.shade900,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: SizedBox(
-                                //Contains our text in card
-                                width: 300,
-                                height: 75,
-                                child: Center(
-                                  child: Text(
-                                    _data[index][2]
-                                        .toString(), // name of cities
-                                    style: TextStyle(
-                                        fontSize: 30,
-                                        fontFamily: 'Poppins',
-                                        color: Colors.white),
-                                  ),
+                                    ),
+                                    SizedBox(height: 20),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          _data[index][2].toString().toUpperCase(),
+                                          style: kTitleTextstyle,
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 20),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          "TRAVEL REQUIREMENTS:",
+                                          style: kTitleTextstyle3,
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 20),
+                                    Text(_data[index][3], style: kTitleTextstyle2),
+                                    SizedBox(height: 10),
+                                    //Travel Requirements card
+                                    TravelCard(
+                                      text: _data[index][4].toString(),
+                                      image: "assets/images/2.png",
+                                      title: "Requirement # 1",
+                                    ),
+                                    TravelCard(
+                                      text: _data[index][5].toString(),
+                                      image: "assets/images/1.png",
+                                      title: "Requirement # 2",
+                                    ),
+                                    TravelCard(
+                                      text: _data[index][6].toString(),
+                                      image: "assets/images/5.png",
+                                      title: "Requirement # 3",
+                                    ),
+                                    TravelCard(
+                                      text: _data[index][7].toString(),
+                                      image: "assets/images/3.png",
+                                      title: "Requirement # 4",
+                                    ),
+                                    SizedBox(height: 30),
+                                  ],
                                 ),
                               ),
                             ),
                             );
-                      }
-                      ),
-                      ),
+                          }
+                          ),
+                          );
+                        },
+                        //create cards for the cities
+                        child: new Card(
+                          elevation: 20,
+                          color: Colors.teal.shade900,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: SizedBox(
+                            //Contains our text in card
+                            width: 300,
+                            height: 75,
+                            child: Center(
+                              child: Text(
+                                _data[index][2].toString(), // name of cities
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
             ],
             ),
-            );
+      ),
+    );
   }
 }
